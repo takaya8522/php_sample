5c07831af223c382b9ea159c3dd8eeee4ceb209f
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-02-12 17:21:37 +0900

    switch文追加

---
+++ switch1.php
@@ -0,0 +1,16 @@
+<?php
+$rank = '丙';
+switch($rank) {
+  case '甲':
+    print '大変良いです。';
+    break;
+  case '乙':
+    print '良いです。';
+    break;
+  case '丙':
+    print 'もう少しがんばりましょう。';
+    break;
+  default:
+    print '???';
+    break;
+}
