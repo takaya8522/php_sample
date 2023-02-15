5cdd768e79aa9fe605e7d0fd609b4dc552031eb0
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-02-15 18:02:44 +0900

    ksort関数追加

--- part5/sort.php
+++ part5/sort.php
@@ -7,3 +7,5 @@ print_r($data);
 $data2 = ['Tennis' => 1, 'Swimming' => 1, 'Soccer' => 11, 'Baseball' => 9];
 asort($data2, SORT_NUMERIC);
 print_r($data2);
+ksort($data2, SORT_STRING);
+print_r($data2);
