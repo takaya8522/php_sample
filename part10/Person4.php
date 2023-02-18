ee9b5cca4f9ae46771971823415d89857e05dd4b
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-02-18 20:44:59 +0900

    Person4.php不具合修正

--- part10/Person4.php
+++ part10/Person4.php
@@ -5,7 +5,7 @@ class Person {
 
   public function __construct(string $firstName, string $lastName) {
     $this->firstName = $firstName;
-    $this->lastName;
+    $this->lastName = $lastName;
   }
 
   public function show() :void {
